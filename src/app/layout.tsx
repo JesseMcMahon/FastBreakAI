b5474fc253830ec47ad6e81174259a0e5b3eb795
Jesse McMahon <jessemcmahon17@gmail.com>
2025-10-15 21:43:00 -0400

    Fix initial zoom on smaller iphones

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,8 +29,9 @@ export default function RootLayout({
       <head>
         <meta
           name="viewport"
-          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no, viewport-fit=cover"
         />
+        <meta name="format-detection" content="telephone=no" />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
